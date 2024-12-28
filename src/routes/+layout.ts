6a543ccb26254a6c270ded62914845aa82a26654
ArcOnyx <1ryanc100@gmail.com>
2024-12-28 21:43:09 +1300

    add supabase type defs

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,11 @@
 import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
 import { createClient } from "@supabase/supabase-js";
 import type { LayoutLoad } from "./$types";
+import type { Database } from "$lib/supabase";
 
 export const prerender = true;
 
-const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY)
+const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY)
 
 export const load: LayoutLoad = async () => {
     return {
