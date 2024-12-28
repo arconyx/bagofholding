946aa5e19d9d92411c8d3ba43a59ecebb97d64d5
ArcOnyx <1ryanc100@gmail.com>
2024-12-28 15:01:26 +1300

    login might work?

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,13 @@
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { createClient } from "@supabase/supabase-js";
+import type { LayoutLoad } from "./$types";
+
 export const prerender = true;
+
+const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY)
+
+export const load: LayoutLoad = async () => {
+    return {
+        supabase: supabase
+    }
+}
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { PUBLIC_BASE_PATH } from '$env/static/public';
+	import type { SupabaseClient } from '@supabase/supabase-js';
+
+	let { data } = $props();
+	const supabase = data.supabase;
+
+	async function signInWithDiscord() {
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: 'discord',
+			options: {
+				redirectTo: `${PUBLIC_BASE_PATH}/auth/callback`
+			}
+		});
+		return;
+	}
+</script>
+
+<button onclick={signInWithDiscord}>Login with Discord</button>
