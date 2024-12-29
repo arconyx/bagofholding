55ce48de3b3d31c47ed98db73acd07120f4e7e9e
ArcOnyx <1ryanc100@gmail.com>
2024-12-29 20:54:27 +1300

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,12 @@
 	let { children } = $props();
 </script>
 
+<nav class="p-2">
+	<a href="/collections">Collections</a>
+	|
+	<a href="/auth/login">Login</a>
+	<!-- TODO: Logout -->
+</nav>
+<hr class="mb-4" />
+
 {@render children()}
