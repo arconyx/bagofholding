93d19a3baddb1cf7eadf2c11c343935f8b2d6de5
ArcOnyx <1ryanc100@gmail.com>
2024-12-29 21:30:57 +1300

    dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,11 @@
 	let { children } = $props();
 </script>
 
-<nav class="p-2">
-	<a href="/collections">Collections</a>
-	|
+<nav class="auto flex flex-row pb-2 pl-4 pr-4 pt-2">
+	<a href="/collections" class="flex-grow">Collections</a>
 	<a href="/auth/login">Login</a>
 	<!-- TODO: Logout -->
 </nav>
-<hr class="mb-4" />
+<hr class="mb-4 ml-2 mr-2" />
 
 {@render children()}
--- src/routes/collection/[id]/ItemBox.svelte
+++ src/routes/collection/[id]/ItemBox.svelte
@@ -10,7 +10,7 @@
 
 <li
 	data-item-id={item.id}
-	class="m-2 block flex min-w-48 flex-col border-2 bg-slate-50 p-2 hover:bg-slate-100"
+	class="m-2 block flex min-w-48 flex-col border-2 bg-slate-50 p-2 hover:bg-slate-100 dark:border-slate-700 dark:bg-slate-900 dark:hover:bg-slate-800"
 >
 	<h3 class="flex-inital text-lg">{item.name}</h3>
 	<p class="text-md flex-grow">{item.description}</p>
