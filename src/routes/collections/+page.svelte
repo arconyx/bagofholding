ddfb5958ce40de0ae149afc11cb7462811ac5fbf
ArcOnyx <1ryanc100@gmail.com>
2024-12-29 16:48:52 +1300

    rework a couple of paths

--- src/routes/collections/+page.svelte
+++ src/routes/collections/+page.svelte
@@ -6,8 +6,8 @@
 
 <ul class="pb-4 pt-4 text-lg">
 	{#each data.collections as collection}
-		<a href="/collection/{collection.id}">{collection.name}</a>
+		<a href="../collection/{collection.id}">{collection.name}</a>
 	{/each}
 </ul>
 
-<a href="/collections/new"> New Collection </a>
+<a href="collections/new"> New Collection </a>
