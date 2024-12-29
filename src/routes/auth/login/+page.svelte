db021f65787432ef6764b2616a366bd402b14fd4
ArcOnyx <1ryanc100@gmail.com>
2024-12-30 02:09:00 +1300

    specify full path for redirect

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -9,7 +9,7 @@
 		const { data, error } = await supabase.auth.signInWithOAuth({
 			provider: 'discord',
 			options: {
-				redirectTo: `${base}/collections`
+				redirectTo: `${origin}${base}/collections`
 			}
 		});
 		return;
