c4fd0adabd295169614708e25be4899c5f191a12
ArcOnyx <1ryanc100@gmail.com>
2024-12-30 22:01:20 +1300

    item display page

--- src/lib/ItemBox.svelte
+++ src/lib/ItemBox.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import type { Tables } from '$lib/supabase';
 
 	interface Props {
@@ -8,14 +9,16 @@
 	let { item }: Props = $props();
 </script>
 
-<li
-	data-item-id={item.id}
-	class="m-2 block flex min-w-48 flex-col border-2 bg-slate-50 p-2 hover:bg-slate-100 dark:border-slate-700 dark:bg-slate-900 dark:hover:bg-slate-800"
->
-	<h3 class="flex-inital text-lg">{item.name}</h3>
-	<p class="text-md flex-grow">{item.description}</p>
-	<div class="flex flex-initial flex-row">
-		<span class="flex-auto text-sm text-slate-600">{item.quantity}x</span>
-		<span class="text-sm text-slate-600">{item.unit_bulk * item.quantity} bulk</span>
-	</div>
-</li>
+<a href="{base}/item/{item.id}">
+	<li
+		data-item-id={item.id}
+		class="m-2 flex min-w-48 flex-col border-2 bg-slate-50 p-2 hover:bg-slate-100 dark:border-slate-700 dark:bg-slate-900 dark:hover:bg-slate-800"
+	>
+		<h3 class="flex-inital text-lg">{item.name}</h3>
+		<p class="text-md flex-grow">{item.description}</p>
+		<div class="flex flex-initial flex-row">
+			<span class="flex-auto text-sm text-slate-600">{item.quantity}x</span>
+			<span class="text-sm text-slate-600">{item.unit_bulk * item.quantity} bulk</span>
+		</div>
+	</li>
+</a>
---
+++ src/routes/item/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	let { data } = $props();
+	const { item } = data;
+</script>
+
+<div class="flex flex-col">
+	<h1 class="text-xl">{item.name}</h1>
+	<p class="pb-4">{item.description}</p>
+	<span>Quantity: {item.quantity}</span>
+	<span>
+		Bulk:
+		{#if item.quantity == 1}{item.unit_bulk}
+		{:else}{item.unit_bulk * item.quantity} ({item.unit_bulk} each)
+		{/if}
+	</span>
+	<!-- <span class="capitalize">Type: {item.type}</span> -->
+	<a class="mt-4" href="{base}/bag/{item.bag_id}">View Bag</a>
+</div>
