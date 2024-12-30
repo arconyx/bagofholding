29d3380b6c389674bc5e3309c35149ba33a469f0
ArcOnyx <1ryanc100@gmail.com>
2024-12-30 23:24:10 +1300

    update supabase schema with players

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -172,6 +172,27 @@ export type Database = {
           },
         ]
       }
+      players: {
+        Row: {
+          created_at: string
+          id: string
+          name: string
+          updated_at: string
+        }
+        Insert: {
+          created_at?: string
+          id: string
+          name: string
+          updated_at?: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          name?: string
+          updated_at?: string
+        }
+        Relationships: []
+      }
     }
     Views: {
       [_ in never]: never
