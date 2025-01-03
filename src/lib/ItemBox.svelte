0cafa635c23ad68e8db686d55c8025c3fc137e20
ArcOnyx <1ryanc100@gmail.com>
2025-01-03 18:09:25 +1300

    refactor itembox slightly

--- src/lib/ItemBox.svelte
+++ src/lib/ItemBox.svelte
@@ -9,16 +9,17 @@
 	let { item }: Props = $props();
 </script>
 
-<a href="{base}/item/{item.id}">
-	<li
-		data-item-id={item.id}
-		class="m-2 flex min-w-48 flex-col border-2 bg-slate-50 p-2 hover:bg-slate-100 dark:border-slate-700 dark:bg-slate-900 dark:hover:bg-slate-800"
-	>
-		<h3 class="flex-inital text-lg">{item.name}</h3>
-		<p class="text-md flex-grow">{item.description}</p>
-		<div class="flex flex-initial flex-row">
-			<span class="flex-auto text-sm text-slate-600">{item.quantity}x</span>
-			<span class="text-sm text-slate-600">{item.unit_bulk * item.quantity} bulk</span>
-		</div>
-	</li>
-</a>
+<div
+	class="m-2 border-2 bg-slate-50 p-2 hover:bg-slate-100 dark:border-slate-700 dark:bg-slate-900 dark:hover:bg-slate-800"
+>
+	<a href="{base}/item/{item.id}">
+		<li data-item-id={item.id} class="flex min-w-48 flex-col">
+			<h3 class="flex-inital text-lg">{item.name}</h3>
+			<p class="text-md flex-grow">{item.description}</p>
+			<div class="flex flex-initial flex-row">
+				<span class="flex-auto text-sm text-slate-600">{item.quantity}x</span>
+				<span class="text-sm text-slate-600">{item.unit_bulk * item.quantity} bulk</span>
+			</div>
+		</li>
+	</a>
+</div>
