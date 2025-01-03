0a44a9f9d0181e7c63a7112394d8101f674bf0dd
ArcOnyx <1ryanc100@gmail.com>
2025-01-03 18:36:20 +1300

    refactor new item form

--- src/routes/bag/[id]/items/new/+page.svelte
+++ src/routes/bag/[id]/items/new/+page.svelte
@@ -1,31 +1,29 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
+	import { goto, invalidate } from '$app/navigation';
 	import { page } from '$app/state';
+	import Item from '$lib/forms/Item.svelte';
 
 	let { data } = $props();
 	const supabase = data.supabase;
 
-	async function submit(event: SubmitEvent) {
-		if (event.target == null) {
-			console.error('Invalid event target');
-			throw new Error('Invalid input');
-		}
+	async function onSuccess() {
+		const url = page.url.searchParams.get('redir') ?? '../';
+		await invalidate(url);
+		await goto(url);
+	}
 
+	async function submit(formElement: HTMLFormElement, event: SubmitEvent) {
 		event.preventDefault();
-
-		// @ts-ignore
-		const form = new FormData(event.target);
+		const form = new FormData(formElement);
 
 		let quantity = (form.get('quantity') ?? -1) as number;
 		if (quantity < 1) {
-			// TODO: Error properly
-			return false;
+			return 'Quantity must be at least one';
 		}
 
 		let bulk = (form.get('bulk') ?? -1) as number;
 		if (bulk < 0) {
-			// TODO: Error properly
-			return false;
+			return 'Bulk cannot be negative.';
 		}
 
 		const { data, error: sub_error } = await supabase.from('items').insert({
@@ -37,36 +35,13 @@
 		});
 
 		if (sub_error != null) {
-			// TODO: Error handling
 			console.error('Failed to create item', sub_error, data);
-			return false;
+			return 'Unable to create item';
 		}
 
-		const redirect_to = page.url.searchParams.get('redir') ?? '../';
-		goto(redirect_to);
-
-		return false;
+		return null;
 	}
 </script>
 
 <h1 class="text-xl">Add Item</h1>
-
-<form method="POST" onsubmit={submit}>
-	<label>
-		Name
-		<input name="name" type="text" required />
-	</label>
-	<label>
-		Description
-		<input name="description" type="text" />
-	</label>
-	<label>
-		Quantity
-		<input name="quantity" type="number" step="1" pattern="\d+" defaultValue="1" required />
-	</label>
-	<label>
-		Bulk per unit
-		<input name="bulk" type="number" step="0.1" defaultValue="1" required />
-	</label>
-	<button>Create</button>
-</form>
+<Item {submit} {onSuccess} submitLabel="Create" item={null} />
