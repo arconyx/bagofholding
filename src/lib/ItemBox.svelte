8f693db4e4f9bc8ad2fe31f0249d0fb697cccc9f
ArcOnyx <1ryanc100@gmail.com>
2025-01-06 17:10:18 +1300

    more improvements to item box formatting

--- src/lib/ItemBox.svelte
+++ src/lib/ItemBox.svelte
@@ -12,8 +12,8 @@
 <div
 	class="m-2 border-2 bg-slate-50 hover:bg-slate-100 dark:border-slate-700 dark:bg-slate-900 dark:hover:bg-slate-800"
 >
-	<a class="block h-full max-h-full w-full max-w-full" href="{base}/item/{item.id}">
-		<li data-item-id={item.id} class="flex min-w-44 flex-col p-2">
+	<a class="block h-full w-full" href="{base}/item/{item.id}">
+		<li data-item-id={item.id} class="flex h-full min-w-44 flex-col p-2">
 			<h3 class="flex-inital text-lg">{item.name}</h3>
 			<p class="text-md flex-grow">{item.description}</p>
 			<div class="flex flex-initial flex-row">
