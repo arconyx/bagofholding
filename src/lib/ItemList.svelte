f286a0b3eccf663d3d6091ad2f6df00d79ccd61b
ArcOnyx <1ryanc100@gmail.com>
2025-01-12 10:45:08 +1300

    add a-z sort

--- src/lib/ItemList.svelte
+++ src/lib/ItemList.svelte
@@ -6,9 +6,31 @@
 		items: Tables<'items'>[];
 	}
 
-	let { items }: Props = $props();
+	let { items: itemsInput }: Props = $props();
+	 var items: Tables<'items'>[] = $state(itemsInput);
+
+	function sortItems(sort: string | null)
+	{
+	     if (sort === 'name:a-z') {
+		  return items.sort(
+		      (a, b) =>  {
+			   if (a.name > b.name) { return 1}
+			   else if (a.name < b.name) { return -1}
+			   else {return 0}
+		      }
+		  )
+	     } else {
+		  return items
+	     }
+	}
+
+
+	 const sortNameAZ = () => {items = sortItems('name:a-z')} 
 </script>
 
+<div>
+    <button onclick={sortNameAZ}>Sort A-Z</button>
+</div>
 <ul class="grid grid-flow-row auto-rows-min grid-cols-auto-fill-48">
 	{#each items as item}
 		<ItemBox {item} />
